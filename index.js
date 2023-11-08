d6e6114127c6ae091c452e2ad9da3ea10cd1f199
sgeremew <geremews@yahoo.com>
2023-11-07 22:57:11 -0500

    sample page

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+
+// create page and save in variable
+let samplePage = (
+    <div>
+        <h3>Sample Page</h3>
+        <ul>
+            <li>apples</li>
+            <li>bananas</li>
+            <li>dragon fruit</li>
+            <li>pomegranete</li>
+        </ul>
+    </div>
+);
+
 // params ( what it is, where to put it )
 ReactDOM.render(
-    <h1>Hello, everyone!</h1>,
+    samplePage,
     document.querySelector("#root")
 );
